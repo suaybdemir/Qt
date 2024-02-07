66151e3c4ee1e514908c021befb878cb62d63b68
Suayb <byausrimed@gmail.com>
2024-02-07 21:45:47 +0300

    patch

--- Widgets/ButtonExamples/widget.cpp
+++ Widgets/ButtonExamples/widget.cpp
@@ -1,7 +1,33 @@
 #include "widget.h"
+#include <QPushButton>
+#include <QMenu>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
-{}
+{
+    setWindowTitle("QPushButton");
+    resize(500,400);
 
-Widget::~Widget() {}
+
+    QPushButton *button = new QPushButton(this);
+    button->setText("Click Me");
+    button->setGeometry(30,30,130,130);
+    button->setFont(QFont("Times",15));
+    button->setIcon(QIcon(":/image/qt.png"));
+    button->setIconSize(QSize(36,36));
+
+    //add popup menu
+    QMenu *menu = new QMenu();
+    menu->setFont(QFont("Sanserif",14));
+    menu->setStyleSheet("background-color:yellow");
+    menu->addAction("Copy");
+    menu->addAction("Cut");
+    menu->addAction("Paste");
+    button->setMenu(menu);
+
+}
+
+Widget::~Widget() {
+
+
+}
