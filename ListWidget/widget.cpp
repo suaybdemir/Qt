99610aa1d4ebeb05ecd27bc4f11061d7da0e58f2
suaybdemir <byausrimed@gmail.com>
2024-02-16 19:56:31 +0300

    Added List Widget

--- ListWidget/widget.cpp
+++ ListWidget/widget.cpp
@@ -1,7 +1,25 @@
 #include "widget.h"
+#include <QVBoxLayout>
+#include<QListWidget>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
-{}
+{
+    resize(500,100);
+
+    QVBoxLayout *vbox = new QVBoxLayout(this);
+
+    QListWidget *listWidget = new QListWidget();
+    listWidget->setFont(QFont("TImes",14));
+    listWidget->setStyleSheet("background-color:yellow");
+
+    listWidget->insertItem(0,"C++");
+    listWidget->insertItem(1,"Python");
+    listWidget->insertItem(2,"Java");
+    listWidget->insertItem(3,"C#");
+
+    vbox->addWidget(listWidget);
+
+}
 
 Widget::~Widget() {}
