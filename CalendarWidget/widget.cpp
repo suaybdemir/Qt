b87db2565437d96aa442e6d0883c45bfa1704856
suaybdemir <byausrimed@gmail.com>
2024-02-16 20:27:19 +0300

    Added Calendar Widget

---
+++ CalendarWidget/widget.cpp
@@ -0,0 +1,30 @@
+#include "widget.h"
+#include <QVBoxLayout>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    resize(500,100);
+    QVBoxLayout *vbox = new QVBoxLayout(this);
+
+    calendar = new QCalendarWidget();
+    calendar->setGridVisible(true);
+    calendar->setStyleSheet("background-color:green");
+    connect(calendar,SIGNAL(selectionChanged()),this,SLOT(selectDate()));
+
+
+    label = new QLabel("Hello");
+    label->setFont(QFont("Times",14));
+    label->setStyleSheet("color:red");
+
+
+    vbox->addWidget(calendar);
+}
+
+Widget::~Widget() {}
+
+void Widget::selectDate()
+{
+    QString dateSelected = calendar->selectedDate().toString();
+    label->setText(dateSelected);
+}
